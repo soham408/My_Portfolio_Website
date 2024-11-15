7884f66c0b812dad0869a289bd0dd41e403a7bb6
Soham Bambade <bambadesoham408@gmail.com>
2024-11-15 12:32:52 +0000

    15 nov

--- Portfolio_Using_React/src/App.jsx
+++ Portfolio_Using_React/src/App.jsx
@@ -5,11 +5,9 @@ import './App.css'
 import Home from './components/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Home />
     </>
   )
 }
--- Portfolio_Using_React/src/components/Home/Home.jsx
+++ Portfolio_Using_React/src/components/Home/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 export default function Home(){
     return (
+        
         <h1>soham</h1>
-
+        
+        /* <Link>
+        <img src="https://images.pexels.com/photos/28948420/pexels-photo-28948420/free-photo-of-gothic-cathedral-reflection-in-puddle-on-cobblestone.jpeg" alt="" srcset="" />
+        </Link > */
+        
+        
     )
 
 }
