bfb4a16ed2c92824b24afabb14a87e52add98e53
Soham Bambade <bambadesoham408@gmail.com>
2024-11-22 11:44:36 +0000

    imgset

--- Portfolio_Using_React/src/components/Techskills/Techskills.jsx
+++ Portfolio_Using_React/src/components/Techskills/Techskills.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
-import Images from './assets/html.jpg'
+// import Images from './src/assets/Images/html.jpg'
 
 export default function Techskills() {
     return (
         
             <ul>
-                <li>
-                    {/* <img src='html.jpg' alt="/no image" /> */}
-                    <img src="Images" alt="/no image" />
-                    
+                <h1 className='text-6xl font-serif text-center pt-[100px] '>Technical Skills</h1>
+                <li className='flex space-x-6 justify-center pt-[250px] '>
+
+                     {/* From Public Folder (Path Way) */}
+
+                    {/* <img src='html.jpg' alt="/no image" />
+                    <img src='CSS.png' alt="/no image" />
+                    <img src='JS.png' alt="/no image" />
+                    <img src='React.png' alt="/no image" />
+                    <img src='git.png' alt="/no image" />
+                    <img src='github.png' alt="/no image" />
+                    <img src='sql.png' alt="/no image" />
+                    <img src='vb.png' alt="/no image" />
+                    <img src='vscode.png' alt="/no image" /> */}
+
+                     {/* From Google Drive :- */}
+
+                            <img className='w-[100px] h-[100px] transform hover:rotate-45 transition duration-300 h-full' 
+                            src="https://drive.google.com/thumbnail?id=1iLtVySfmUTFjXjm-fhY_RAMkd4-ujL6I" alt="" srcset="" />
+                            <img className='w-[100px] h-[100px] transform hover:rotate-45 transition duration-300 h-full' 
+                            src="https://drive.google.com/thumbnail?id=1W5X4-m5n7PJqXsO91y-3usRSGwXeFT70" alt="" srcset="" />
+                            <img className='w-[100px] h-[100px] transform hover:rotate-45 transition duration-300 h-full'
+                            src="https://drive.google.com/thumbnail?id=1j3Dp0pFzZQ1_vZB0gGuanZ7dUc31BZ10" alt="" srcset="" />
+                            <img className='w-[100px] h-[100px] transform hover:rotate-45 transition duration-300 h-full'
+                            src="https://drive.google.com/thumbnail?id=1diKLEKJyjHHW7Yt0-64Ima2NoB6eBiag" alt="" srcset="" />
+                            <img className='w-[100px] h-[100px] transform hover:rotate-45 transition duration-300 h-full'
+                            src="https://drive.google.com/thumbnail?id=10s82Mzs3hzf7snaQpGcT5j-vZ_8I90Ak" alt="" srcset="" />
+                            <img className='w-[100px] h-[100px] transform hover:rotate-45 transition duration-300 h-full'
+                            src="https://drive.google.com/thumbnail?id=1oSfQdi5l3ygK82YAVIaLv3sSTR8iNcDj" alt="" srcset="" />
+                            <img className='w-[100px] h-[100px] transform hover:rotate-45 transition duration-300 h-full'
+                            src="https://drive.google.com/thumbnail?id=1-ugKRhfGZ0YcojgBoJTBqYa6SNNvBBlH" alt="" srcset="" />
+                            <img className='w-[100px] h-[100px] transform hover:rotate-45 transition duration-300 h-full'
+                            src="https://drive.google.com/thumbnail?id=11wdQG5IL1ZABEUnwmljsV5IdaiXvpjOo" alt="" srcset="" />
+                            <img className='w-[100px] h-[100px] transform hover:rotate-45 transition duration-300 h-full' 
+                            src="https://drive.google.com/thumbnail?id=14noxHp-JB3UcniwkEEtHd_nkL8QmTW4z" alt="" srcset="" />
                 </li>
             </ul>
         
