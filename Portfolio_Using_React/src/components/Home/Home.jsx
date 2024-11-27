ac1be7464e81a316f9420e4a5e41ef3ddd4189ad
soham408 <bambadesoham408@hmail.com>
2024-11-27 17:37:34 +0530

    last sec

--- Portfolio_Using_React/src/components/Home/Home.jsx
+++ Portfolio_Using_React/src/components/Home/Home.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
+// import imgHome from './Images/HomeComponent.png'
 
 export default function Home(){
     return (
         // mix-blend-multiply
         <div className='flex items-center justify-center pt-[80px] pb-[100px]'>
-          {/* <div className='w-[90%] h-[60vh] bg-white'>Hello</div> */}
       <img className='w-[90%] h-[60vh] bg-none rounded-xl transition-all duration-300 hover:scale-110'
             src="https://drive.google.com/thumbnail?id=1COxYMHD0rrkpLi_WXH6Y4Q7vpa7PI_pR"  alt="" />
-            
+             {/* src={imgHome} alt="" srcset="" /> */}
            </div>
             
             
